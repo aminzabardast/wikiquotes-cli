06bd404b9b59becad5e2ce90cf916224c3626583
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-08-30 19:04:08 +0300

    Fixed Commander structure Refs #3

--- node_js/index.js
+++ node_js/index.js
@@ -1,3 +1,36 @@
 #!/usr/bin/env node
 
-console.log('This would be the CLI.')
+// Declaring program variable
+const { Command } = require('commander');
+const program = new Command();
+
+// Add Program Options and Version Information
+program
+    .version('0.1.0')
+    .option('-j, --json-output', 'Output in JSON format')
+
+// Add Commands
+program
+    .command('random')
+    .alias('rand')
+    .description('Load a random quote')
+    .option('-a, --additional-information', 'Output Additional Information')
+    .action((options) => {
+        console.log('--random--')
+        console.log(`Command Options: ${JSON.stringify(options)}`)
+        console.log(`Program Options: ${JSON.stringify(program.opts())}`)
+    });
+
+program
+    .command('author <name>')
+    .description('Load a random quote form a given author')
+    .option('-a, --additional-information', 'Output Additional Information')
+    .action((name, options) => {
+        console.log('--author--')
+        console.log(`Name: ${name}`);
+        console.log(`Command Options: ${JSON.stringify(options)}`)
+        console.log(`Program Options: ${JSON.stringify(program.opts())}`)
+    }).addHelpText('after', `\nExamples:\n  $ wikiquotes author "Bill Gates"`
+);
+
+program.parse();
