69a3eb9e81fb4faaf301d8143cd421ca6ef120d4
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-08-31 15:29:48 +0300

    Searching for individuals and loading their page Refs #4

--- node_js/index.js
+++ node_js/index.js
@@ -1,7 +1,67 @@
 #!/usr/bin/env node
 
 // Declaring program variable
+const axios = require('axios').default
 const { Command } = require('commander')
+const { capitalize, map, clone, join, without } = require('lodash')
+const cheerio = require('cheerio')
+
+const generateAllCases = (numberOfWords, phrase, result, counter=0, arr=[]) => {
+    {
+        if (counter === numberOfWords) {
+            phrase = phrase.split(' ')
+            phrase = map(arr, (item, idx) => {
+                return item ? capitalize(phrase[idx]) : phrase[idx]
+            })
+            phrase = join(phrase, ' ')
+            result.push(phrase)
+        } else {
+            let arr1 = clone(arr)
+            arr1.push(0)
+            generateAllCases(numberOfWords, phrase, result, counter + 1, arr1)
+            let arr2 = clone(arr)
+            arr2.push(1);
+            generateAllCases(numberOfWords, phrase, result, counter + 1, arr2)
+        }
+    }
+}
+
+const searchAuthor = async name => {
+    let parts = name.toLowerCase().split(' ')
+    const wordCount = parts.length
+    const result = []
+    generateAllCases(wordCount, name.toLowerCase(), result)
+    return new Promise((resolve, reject) => {
+        axios.get(`https://en.wikiquote.org/w/api.php?action=query&format=json&indexpageids=1&iwurl=1&titles=${join(result, '|')}&redirects=1&utf8=1`)
+            .then( response => {
+                const pageIds = response.data.query.pageids
+                const targets = without(map(pageIds, item => parseInt(item) > 0 ? item : undefined), undefined)
+                if (targets.length) {
+                    resolve(targets)
+                } else {
+                    reject('Author not found!')
+                }
+            })
+            .catch( error => {
+                reject(error)
+            })
+    })
+}
+
+const getQuotes = async pageId => {
+    return new Promise((resolve, reject) => {
+        axios.get(`https://en.wikiquote.org/w/api.php?action=query&format=json&export=1&exportnowrap=1&pageids=${pageId}&utf8=1`)
+            .then( response => {
+                const data = response.data
+                const $ = cheerio.load(data)
+                resolve($('text').text())
+            })
+            .catch( error => {
+                reject(error)
+            })
+    })
+}
+
 const program = new Command()
 
 // Add Program Options and Version Information
@@ -22,15 +82,24 @@ program
     })
 
 program
-    .command('author <name>')
-    .description('Load a random quote form a given author')
+    .command(' individual <name>')
+    .alias('ind')
+    .description('Load a random quote form a given individual')
     .option('-a, --additional-information', 'Output Additional Information')
     .action((name, options) => {
-        console.log('--author--')
-        console.log(`Name: ${name}`);
+        console.log('--individual--')
+        console.log(`name: ${name}`);
+        searchAuthor(name)
+            .then(pageIds => getQuotes(pageIds[0])
+                .then(response => {
+                    console.log('Wiki Content:')
+                    console.log(response)
+                })
+            )
+            .catch(error => console.log(error))
         console.log(`Command Options: ${JSON.stringify(options)}`)
         console.log(`Program Options: ${JSON.stringify(program.opts())}`)
-    }).addHelpText('after', `\nExamples:\n  $ wikiquotes author "Bill Gates"`
+    }).addHelpText('after', `\nExamples:\n  $ wikiquotes ind "Bill Gates"`
 )
 
 program.parse();
